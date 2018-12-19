ad3f8e276c0278dd9a9e8d83a3f3878e5d3a25e3
Babafemi <Babafemi@BABAFEMI>
2018-12-19 01:53:33 +0100

    go working

---
+++ quickgotutorial/packages/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/thesoftwaremasons/quickgotutorial/packages/util"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.5))
+	fmt.Println(math.Cosh(2.7))
+	fmt.Println(util.Reverse("Hello"))
+}
---
+++ quickgotutorial/var/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name = "Femi"
+	//var age int32 = 0
+	//cool := "time"
+	soon, bye := "soon", "bye"
+
+	//fmt.Println(name, age)
+	fmt.Printf("%T\n", soon)
+	fmt.Printf("%T\n", bye)
+
+}
